e2b21271fe5143ffeb135383ed5360df4c6e05da
ChrisRothove <vacantorange@gmail.com>
2025-03-26 19:01:48 -0500

    more setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@pages': '/src/components/pages',
+      '@utils': '/src/utils',
+      '@types': '/src/types',
+      '@styles': '/src/styles'
+    },
+  },
 })
