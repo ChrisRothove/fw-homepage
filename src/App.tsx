efeb586081cc31c834430e710b29fc58de6efc94
ChrisRothove <vacantorange@gmail.com>
2025-04-23 23:54:24 -0500

    added images and set up nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import me from '../public/me.jpg'
 import '@styles/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='card main-card'>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={me} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1 className='with-sub-head'>Chris Rothove</h1>
+      <h2 className='sub-head with-sub-head'>@Featherwalk</h2>
+      <h3 className='folio sub-head'>Software Dev | Illustration | Game Design</h3>
+      <nav className='vertical-buttons'>
+        <button>Web Projects</button>
+        <button>Game Design</button>
+        <button>Brand Wars</button>
+      </nav>
+      <section>
+        <h2 className='section-header'>Welcome!</h2>
+        <p>I'm a software developer and artist based in the Kansas City Missouri area. I use this page as a hub for various projects that I'm working on (I tend to have several of these). Feel free to peruse my portfolio, and thanks for visiting my page today!</p>
+      </section>
+    </div>
   )
 }
 
